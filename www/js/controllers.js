249839bb260ab1b9aaf1daa25817412c01084d38
Noah Yarian <noah.yarian@gmail.com>
2015-09-23 04:15:15 -0500

    starting google OAuth setup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,7 +43,20 @@ angular.module('samplePad.controllers', [])
 
   .controller('mainController', ['$scope', function ($scope) {
 
-    $scope.message = "hello";
+    $scope.onSignIn = function (googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
+
+    $scope.signOut = function () {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+      });
+    }
 
   }])
 
