a757384f84f141b5578e4711907580ac567b3965
Noah Yarian <noah.yarian@gmail.com>
2015-09-22 21:43:56 -0500

    changing default sound sources to s3

--- app/board.js
+++ app/board.js
@@ -12,62 +12,62 @@ var BoardSchema   = new Schema({
     default: [
       {
         "hotkey": "1",
-        "src": "/sounds/808/BD/BD0075.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/BD/BD0075.wav",
         "name": "808 Bass Drum"
       },
       {
         "hotkey": "2",
-        "src": "/sounds/808/SD/SD0010.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/SD/SD0010.wav",
         "name": "808 Snare Drum"
       },
       {
         "hotkey": "3",
-        "src": "/sounds/808/CH/CH.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/CH/CH.wav",
         "name": "808 Closed Hat"
       },
       {
         "hotkey": "4",
-        "src": "/sounds/808/CP/CP.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/CP/CP.wav",
         "name": "808 Hand Clap"
       },
       {
         "hotkey": "Q",
-        "src": "/sounds/808/LT/LT00.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/LT/LT00.wav",
         "name": "808 Low Tom"
       },
       {
         "hotkey": "W",
-        "src": "/sounds/808/MT/MT00.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/MT/MT00.wav",
         "name": "808 Mid Tom"
       },
       {
         "hotkey": "E",
-        "src": "/sounds/808/HT/HT00.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/HT/HT00.wav",
         "name": "808 Hi Tom"
       },
       {
         "hotkey": "R",
-        "src": "/sounds/808/RS/RS.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/RS/RS.wav",
         "name": "808 Rim Shot"
       },
       {
         "hotkey": "A",
-        "src": "/sounds/808/LC/LC00.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/LC/LC00.wav",
         "name": "808 Low Conga"
       },
       {
         "hotkey": "S",
-        "src": "/sounds/808/MC/MC00.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/MC/MC00.wav",
         "name": "808 Mid Conga"
       },
       {
         "hotkey": "D",
-        "src": "/sounds/808/HC/HC00.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/HC/HC00.wav",
         "name": "808 Hi Conga"
       },
       {
         "hotkey": "F",
-        "src": "/sounds/808/CB/CB.wav",
+        "src": "http://assets.noahyarian.com/sounds/808/CB/CB.wav",
         "name": "808 Cow Bell"
       }
     ]
